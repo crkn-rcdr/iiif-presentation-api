6c5fa1970eafeea6733e8505f235001475872a93
Brittny Lapierre <brittny.lapierre@gmail.com>
2023-09-06 15:55:23 -0400

    Routes for iif complete

--- README.md
+++ README.md
@@ -14,9 +14,4 @@ docker logs fastify-iiif-presentation-api_apiserver_1 --follow
 
 
 TODO:
-GET /iiif/<collection_id>/collection
-GET /iiif/<manifest_id>/manifest
-GET /iiif/<manifest_id>/canvas/<canvas_id_1>/<canvas_id_2>
-GET /iiif/<manifest_id>/annotationpage/<canvas_id_1>/<canvas_id_2>/<annotationpage_id>
-GET /iiif/<manifest_id>/annotation/<canvas_id_1>/<canvas_id_2>/<annotationpage_id>/<annotation_id>
 PUT, POST, DELETE http://localhost:8080/manifest
--- services/iiif.js
+++ services/iiif.js
@@ -2,17 +2,27 @@
 
 //const schemas = require('../schemas/iiif')
 
+
 module.exports = async function (fastify, opts) {
 
   fastify.setNotFoundHandler(function (request, reply) {
     reply
       .code(404)
       .type('application/json')
-      .send({ message: 'Requested todo item does not exist' })
+      .send({ message: 'Requested route does not exist' })
   })
 
+  fastify.get(
+    '/:id/collection',
+    //{ schema: schemas.findOne },
+    async function (request, reply) {
+      const client = await fastify.pg.connect()
+      return { error: "Endpoint not implemented" }
+    }
+  )
+
   /*fastify.get(
-    '/',
+    '/manifest',
     { schema: schemas.findAll },
     async function (request, reply) {
       const limit = parseInt(request.query.limit) || 0
@@ -28,16 +38,16 @@ module.exports = async function (fastify, opts) {
   )*/
 
   fastify.get(
-    '/:id/manifest',
+    '/:manifestId/manifest',
     //{ schema: schemas.findOne },
     async function (request, reply) {
       const client = await fastify.pg.connect()
       try {
         const { rows } = await client.query(
-          "SELECT * FROM manifests WHERE manifest_json->>'id' =$1", ['http://10.5.0.5:5000/iiif/' + request.params.id + '/manifest'],
+          "SELECT * FROM manifests WHERE manifest_json->>'id' =$1", ['http://10.5.0.5:5000/iiif/' + request.params.manifestId + '/manifest'],
         )
         // Note: avoid doing expensive computation here, this will block releasing the client
-        return rows.length ? rows[0]['manifest_json'] : { error : "item not found." }
+        return rows.length ? rows[0]['manifest_json'] : { error : "Not found." }
       } finally {
         // Release the client immediately after query resolves, or upon error
         client.release()
@@ -45,41 +55,107 @@ module.exports = async function (fastify, opts) {
     }
   )
 
-  /*fastify.post(
-    '/',
-    { schema: schemas.insertOne },
+  fastify.get(
+    '/:manifestId/canvas/:canvasIdPrefix/:canvasIdSuffix',
+    //{ schema: schemas.findOne },
     async function (request, reply) {
-      return this.mongo.db.collection('todo').insertOne(
-        Object.assign(request.body, {
-          timestamp: this.timestamp(),
-          done: false
-        })
-      )
-    }
-  )
+      const client = await fastify.pg.connect()
 
-  fastify.put(
-    '/:name',
-    { schema: schemas.updateOne },
-    async function (request, reply) {
-      return this.mongo.db
-        .collection('todo')
-        .findOneAndUpdate(
-          { name: request.params.name },
-          { $set: { done: request.body.done } }
+      let manifest;
+      try {
+        const { rows } = await client.query(
+          "SELECT * FROM manifests WHERE manifest_json->>'id' =$1", ['http://10.5.0.5:5000/iiif/' + request.params.manifestId + '/manifest'],
         )
+        // Note: avoid doing expensive computation here, this will block releasing the client
+        if(rows.length) manifest = rows[0]['manifest_json']
+      } finally {
+        // Release the client immediately after query resolves, or upon error
+        client.release()
+      }
+
+      let canvas = { 'error' : 'Not found' }
+      for (let item of manifest['items']) {
+        if (item['id'] == 'http://10.5.0.5:5000/iiif/'+request.params.manifestId+'/canvas/'+request.params.canvasIdPrefix+'/'+request.params.canvasIdSuffix) {
+          canvas = item
+          break
+        }
+      }
+      return canvas
     }
   )
 
-  fastify.delete(
-    '/:name',
-    { schema: schemas.deleteOne },
+  fastify.get(
+    '/:manifestId/annotationpage/:canvasIdPrefix/:canvasIdSuffix/:annotationPageName',
+    //{ schema: schemas.findOne },
     async function (request, reply) {
-      return this.mongo.db
-        .collection('todo')
-        .deleteOne({ name: request.params.name })
+      const client = await fastify.pg.connect()
+
+      let manifest;
+      try {
+        const { rows } = await client.query(
+          "SELECT * FROM manifests WHERE manifest_json->>'id' =$1", ['http://10.5.0.5:5000/iiif/' + request.params.manifestId + '/manifest'],
+        )
+        // Note: avoid doing expensive computation here, this will block releasing the client
+        if(rows.length) manifest = rows[0]['manifest_json']
+      } finally {
+        // Release the client immediately after query resolves, or upon error
+        client.release()
+      }
+
+      let annotationpage = { 'error' : 'Not found.' }
+      for (let item of manifest['items']) {
+        if (item['id'] == 'http://10.5.0.5:5000/iiif/'+request.params.manifestId+'/canvas/'+request.params.canvasIdPrefix+'/'+request.params.canvasIdSuffix){
+          for (let canvasItem of item['items']){
+            if (canvasItem['id'] == 'http://10.5.0.5:5000/iiif/'+request.params.manifestId+'/annotationpage/'+request.params.canvasIdPrefix+'/'+request.params.canvasIdSuffix+'/'+request.params.annotationPageName){
+              annotationpage = canvasItem
+              break
+            }
+          }
+        }
+      }
+      return annotationpage
     }
-  )*/
+  )
+
+
+  fastify.get(
+    '/:manifestId/annotation/:canvasIdPrefix/:canvasIdSuffix/:annotationPageName/:annotationId',
+    //{ schema: schemas.findOne },
+    async function (request, reply) {
+      const client = await fastify.pg.connect()
+
+      let manifest;
+      try {
+        const { rows } = await client.query(
+          "SELECT * FROM manifests WHERE manifest_json->>'id' =$1", ['http://10.5.0.5:5000/iiif/' + request.params.manifestId + '/manifest'],
+        )
+        // Note: avoid doing expensive computation here, this will block releasing the client
+        if(rows.length) manifest = rows[0]['manifest_json']
+      } finally {
+        // Release the client immediately after query resolves, or upon error
+        client.release()
+      }
+
+      let annotation={ 'error' : 'Not found.' }
+      if(manifest) {
+        for (let item of manifest['items']) {
+          if (item['id'] == 'http://10.5.0.5:5000/iiif/'+request.params.manifestId+'/canvas/'+request.params.canvasIdPrefix+'/'+request.params.canvasIdSuffix) {
+            for (let canvasItem of item['items']){
+              if (canvasItem['id'] == 'http://10.5.0.5:5000/iiif/'+request.params.manifestId+'/annotationpage/'+request.params.canvasIdPrefix+'/'+request.params.canvasIdSuffix+'/'+request.params.annotationPageName) {
+                for (let annotationpageItem of canvasItem['items']) {
+                  if (annotationpageItem['id'] == 'http://10.5.0.5:5000/iiif/'+request.params.manifestId+'/annotation/'+request.params.canvasIdPrefix+'/'+request.params.canvasIdSuffix+'/'+request.params.annotationPageName+'/'+request.params.annotationId) {
+                    annotation = annotationpageItem
+                    break
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      return annotation
+    }
+  )
 }
 
 module.exports.autoPrefix = '/iiif'
